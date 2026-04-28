187e8fc827d44286d747e60c4f347dd166e0a44c
lchern <leonid.chern@gmail.com>
2026-04-28 10:44:57 +0200

    28.04.2026 added FPS limit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -11,6 +13,8 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 
