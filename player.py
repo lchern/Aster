78d70e590f1c66902b58fd892d2acc763e23ee87
lchern <leonid.chern@gmail.com>
2026-04-30 18:25:31 +0400

    30.04.2026 added cooldown timer for a gun

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -31,12 +32,17 @@ class Player(circleshape.CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
-        new_shot = Shot(self.position, SHOT_RADIUS)
-        new_shot.velocity = (
-            pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        )
+        if self.cooldown_timer > 0:
+            pass
+        else:
+            new_shot = Shot(self.position, SHOT_RADIUS)
+            new_shot.velocity = (
+                pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            )
+            self.cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
 
     def update(self, dt):
+        self.cooldown_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
